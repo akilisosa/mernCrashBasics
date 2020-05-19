d4834c8a2f481309ac1f419ab742ef3570b3bd15
Akili Sosa <akiilsosa@gmail.com>
2020-05-19 14:39:00 -0400

    token basic

---
+++ routes/api/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth')
+// @route    GET  api/auth
+// @desc     Test route
+// @acess    Public
+router.get('/', auth, (req, res) => res.send('auth route'));
+
+module.exports = router;
